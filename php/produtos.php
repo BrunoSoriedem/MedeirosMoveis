d4251ac761d277fbd3c26aefc4fbdfe5360ad294
Bruno Medeiros <brunorafamed.com@gmail.com>
2025-05-22 22:54:55 -0300

    vou explodir mano

---
+++ php/produtos.php
@@ -0,0 +1,11 @@
+<?php
+$filmes = [
+    ["funcionario" => "Paulo Medeiros", "capa" => "imagens/1.jpg" ],    //paulo
+    ["funcionario" => "Silvia Medeiros", "capa" => "imagens/2.jpg" ],        //silvia
+    ["funcionario" => "Felipe Medeiros", "capa" => "imagens/3.jpg" ],          //felipe
+    ["funcionario" => "Nataly Carvalho", "capa" => "imagens/4.jpg" ],           //nataly
+    ["funcionario" => "Larissa Araújo", "capa" => "imagens/5.jpg" ], //larissa
+    ["funcionario" => "Demolidor", "capa" => "imagens/6.jpg" ],         //nei
+    ["funcionario" => "Edicarlo Oliveira", "capa" => "imagens/7.jpg" ]       //edicarlos
+];
+?>
