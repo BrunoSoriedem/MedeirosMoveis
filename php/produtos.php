cf84d63f8d058814bb4d8157b04af5d05ca6b98b
Bruno Medeiros <brunorafamed.com@gmail.com>
2025-06-03 22:15:15 -0300

    começando o 'HOME'

--- php/produtos.php
+++ php/produtos.php
@@ -1,11 +1,10 @@
 <?php
 $filmes = [
-    ["funcionario" => "Paulo Medeiros", "capa" => "imagens/1.jpg" ],    //paulo
-    ["funcionario" => "Silvia Medeiros", "capa" => "imagens/2.jpg" ],        //silvia
-    ["funcionario" => "Felipe Medeiros", "capa" => "imagens/3.jpg" ],          //felipe
-    ["funcionario" => "Nataly Carvalho", "capa" => "imagens/4.jpg" ],           //nataly
-    ["funcionario" => "Larissa Araújo", "capa" => "imagens/5.jpg" ], //larissa
-    ["funcionario" => "Demolidor", "capa" => "imagens/6.jpg" ],         //nei
-    ["funcionario" => "Edicarlo Oliveira", "capa" => "imagens/7.jpg" ]       //edicarlos
+    ["funcionario" => "Paulo Medeiros",    "capa" => "imagens/1.jpg"], //paulo
+    ["funcionario" => "Silvia Medeiros",   "capa" => "imagens/2.jpg"], //silvia
+    ["funcionario" => "Felipe Medeiros",   "capa" => "imagens/3.jpg"], //felipe
+    ["funcionario" => "Nataly Carvalho",   "capa" => "imagens/4.jpg"], //nataly
+    ["funcionario" => "Larissa Araújo",    "capa" => "imagens/5.jpg"], //larissa
+    ["funcionario" => "Claudinei Bezerra", "capa" => "imagens/6.jpg"], //nei
+    ["funcionario" => "Edicarlo Oliveira", "capa" => "imagens/7.jpg"]  //edicarlos
 ];
-?>
